60a9d93fe383e0f8d2a3b2164b96ce77587f3927
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-17 17:08:57 +0200

    Added get, post, README

---
+++ README.md
@@ -0,0 +1,2 @@
+### Quote API
+A simple api built with Express/NodeJS to get and post quotes about tech, etc.
--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+const quotesRouter = express.Router();
+
+app.use("/api/quotes", quotesRouter);
+
+quotesRouter.get("/random", (req, res, next) => {
+  const quote = getRandomElement(quotes);
+  res.send({ quote: quote });
+});
+
+quotesRouter.get("/", (req, res, next) => {
+  if (!req.query.hasOwnProperty("person")) {
+    res.send({ quotes: quotes });
+  } else {
+    const filterQuote = quotes.filter(
+      (element) => element.person === req.query.person
+    );
+    res.send({ quotes: filterQuote });
+  }
+});
+
+quotesRouter.post("/", (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const newQuote = { quote: req.query.quote, person: req.query.person };
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
